29462b4054bbb6ab629053213c69b2d14324ce4b
Krishna Prashanth <62010158+krishnaprashanth246@users.noreply.github.com>
2024-08-18 15:38:39 -0700

    Add multiple API endpoints for different tasks

--- app/app.py
+++ app/app.py
@@ -6,12 +6,26 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.post('/annotate')
-def annotate():
+@app.post('/annotate/tokens')
+def annotate_tokens():
 	data = request.get_json()
 	input_text = data.get('text', '')
 	output_text = get_tokens(input_text)
-	return {'message': 'CoreNLP output: \n' + output_text}
+	return {'message': 'CoreNLP Tokenization: \n' + output_text}
+
+@app.post('/annotate/split')
+def annotate_ssplit():
+	data = request.get_json()
+	input_text = data.get('text', '')
+	output_text = split_sentences(input_text)
+	return {'message': 'CoreNLP Sentence Split: \n' + output_text}
+
+@app.post('/annotate/pos')
+def annotate_pos():
+	data = request.get_json()
+	input_text = data.get('text', '')
+	output_text = parts_of_speech(input_text)
+	return {'message': 'CoreNLP Parts of Speech tagging: \n' + output_text}
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
--- app/corenlp/client.py
+++ app/corenlp/client.py
@@ -3,5 +3,13 @@ import requests
 url = 'http://corenlp:9000/'
 
 def get_tokens(str):
-    response = requests.post(url, data=str, params={'properties':'{"annotators":"tokenize,pos","outputFormat":"json"}'})
+    response = requests.post(url, data=str, params={'properties':'{"annotators":"tokenize","outputFormat":"json"}'})
+    return response.text
+
+def split_sentences(str):
+    response = requests.post(url, data=str, params={'properties':'{"annotators":"ssplit","outputFormat":"json"}'})
+    return response.text
+
+def parts_of_speech(str):
+    response = requests.post(url, data=str, params={'properties':'{"annotators":"pos","outputFormat":"json"}'})
     return response.text
